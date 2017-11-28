148da958f243f2a3e5d02deece4fc50121ff0938
jduggerIIT <jdugger@iit.edu>
2017-11-27 20:00:13 -0600

    Fixed copy-paste erros in vagrant files

--- vagrantprov.sh
+++ vagrantprov.sh
@@ -1,9 +1,8 @@
-+#!/usr/bin/env bash
-+
-+apt-get update
-+apt-get install -y qemu-system-x86
-+apt-get install -y gdb
-+apt-get install -y tmux
-+
-Add a comment to this line
-+echo "set auto-load safe-path /" > /home/vagrant/.gdbinit
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y qemu-system-x86
+apt-get install -y gdb
+apt-get install -y tmux
+
+echo "set auto-load safe-path /" > /home/vagrant/.gdbinit
